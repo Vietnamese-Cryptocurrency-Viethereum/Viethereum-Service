72db14430fdeb35e973f33a5f82e7d222a51b1e6
TrunkinatorV <diabloviet@gmail.com>
2024-09-08 16:32:25 +0700

    ethers-lib.ts
    Signed-off-by: TrunkinatorV <diabloviet@gmail.com>

--- scripts/ethers-lib.ts
+++ scripts/ethers-lib.ts
@@ -3,13 +3,13 @@ import { ethers } from 'ethers'
 /**
  * Deploy the given contract
  * @param {string} contractName name of the contract to deploy
- * @param {Array<any>} args list of constructor' parameters
+ * @param {Array<any>} args list of constructor parameters
  * @param {Number} accountIndex account index from the exposed account
  * @return {Contract} deployed contract
  */
-export const deploy = async (contractName: string, args: Array<any>, accountIndex?: number): Promise<ethers.Contract> => {
+export const deploy = async (contractName: string = 'Viethereum', args: Array<any> = [], accountIndex?: number): Promise<ethers.Contract> => {
 
-  console.log(`deploying ${contractName}`)
+  console.log(`Deploying ${contractName}`)
   // Note that the script needs the ABI which is generated from the compilation artifact.
   // Make sure contract is compiled and artifacts are generated
   const artifactsPath = `browser/contracts/artifacts/${contractName}.json` // Change this for different path
